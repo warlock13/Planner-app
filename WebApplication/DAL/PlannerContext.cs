608b4bcb856052a77e0018f9abb14c4f5b75a3a7
Artem <artemshlemko01@gmail.com>
2019-02-04 03:54:36 +0300

    Fixed datetime2 exception

--- WebApplication/DAL/PlannerContext.cs
+++ WebApplication/DAL/PlannerContext.cs
@@ -1,17 +1,54 @@
 ﻿using WebApplication.Models;
 using System.Data.Entity;
 using System.Data.Entity.ModelConfiguration.Conventions;
+using System;
+using System.Data.SqlTypes;
 
 namespace WebApplication.DAL
 {
     public class PlannerContext : DbContext
     {
+
         public PlannerContext() : base("PlannerContext"){}
         public DbSet<Task> Tasks { get; set; }
         protected override void OnModelCreating(DbModelBuilder modelBuilder)
         {
             modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
         }
+
+        /* public override int SaveChanges()
+        {
+            UpdateDates();
+            return base.SaveChanges();
+        }
+
+        private void UpdateDates()
+        {
+            foreach (var change in ChangeTracker.Entries<Task>())
+            {
+                var values = change.CurrentValues;
+                foreach (var name in values.PropertyNames)
+                {
+                    var value = values[name];
+                    if (value is DateTime)
+                    {
+                        var date = (DateTime)value;
+                        if (date < SqlDateTime.MinValue.Value)
+                        {
+                            values[name] = SqlDateTime.MinValue.Value;
+                        }
+                        else if (date > SqlDateTime.MaxValue.Value)
+                        {
+                            values[name] = SqlDateTime.MaxValue.Value;
+                        }
+                    }
+                }
+            }
+        } */
+
+
     }
+
+
 
 }
