2ad0c50ae9ed513058ddfc493c101e41799b8e69
warlock13 <artemshlemko01@gmail.com>
2019-02-04 04:00:43 +0300

    Fixed datetime2 exception

--- WebApplication/Models/Task.cs
+++ WebApplication/Models/Task.cs
@@ -11,16 +11,22 @@ namespace WebApplication.Models
     public class Task
     {
         public int ID { get; set; }
+        
         [DisplayName("Название")]
         public string Name { get; set; }
+        
         [DisplayName("Описание")]
         [DataType(DataType.MultilineText)]
         public string Description { get; set; }
+        
         [DisplayName("Дата выполнения")]
+        [Column(TypeName = "datetime2")]
         [DisplayFormat(ApplyFormatInEditMode = true, DataFormatString = "{0:yyyy-MM-dd}")]
         public DateTime Date { get; set; }
+        
         [DisplayName("Закрепить")]
         public Boolean isActive { get; set; }
+        
         [DisplayName("Актуально")]
         public Boolean isActual { get; set; }
     }
