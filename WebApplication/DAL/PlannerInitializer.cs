eecedf567387192777d9f3f64213223be4d473b9
Artem <artemshlemko01@gmail.com>
2019-02-04 04:47:15 +0300

    Added DropCreateDatabaseIfModelChanges initializer

--- WebApplication/DAL/PlannerInitializer.cs
+++ WebApplication/DAL/PlannerInitializer.cs
@@ -12,9 +12,9 @@ namespace WebApplication.DAL
         {
             var tesks = new List<Task>
             {
-            new Task{Name="Молоко",Description="Купить в магазине",Date=DateTime.Parse("2005-01-01")},
+            new Task{Name="Молоко",Description="Купить в магазине",Date=DateTime.Parse("2005-01-01"), isActive=true},
             new Task{Name="Хлеб",Description="Заготовить тесто",Date=DateTime.Parse("2025-01-01"), isActual=true},
-            new Task{Name="Уборка",Description="Пропылесосить и помыть пол",Date=DateTime.Parse("2029-01-25"), isActual=true},
+            new Task{Name="Уборка",Description="Пропылесосить и помыть пол",Date=DateTime.Parse("2029-01-25"), isActual=true, isActive=true},
             new Task{Name="Мусор",Description="Вынести мусор и натянуть чистый пакет на урну",Date=DateTime.Parse("2012-01-01")},
             new Task{Name="Ужин",Description="Приготовить что-то легкое на ужин",Date=DateTime.Parse("2030-02-13"), isActual=true}
             };
